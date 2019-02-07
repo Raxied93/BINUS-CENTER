8e36fd1df9f2efdfeab8c3eb9a3ca16845d870f1
Herlambang Rizky <tandasanyu@gmail.com>
2019-02-07 08:51:03 +0700

    Sesi1_WebProg_ASPNET

---
+++ WebSite7/Home.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Home.aspx.cs" Inherits="Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TextBox1" runat="server" ></asp:TextBox>
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server"  ControlToValidate="TextBox1" ErrorMessage="Only Numbers allowed" ValidationExpression="\d+"></asp:RegularExpressionValidator>
+    <br />
+        <asp:Button ID="Button1" runat="server" Text="Hit Me" OnClick="Button1_Click" style="height: 26px" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebSite7/Home.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Home : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    public string s;
+        
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        s = TextBox1.Text;
+        Response.Write("<script>alert('Value : "+s+"')</script>");
+        string url = HttpContext.Current.Request.Url.AbsoluteUri;
+        string user_agent = Request.UserAgent;
+        string userhost = Request.UserHostAddress;
+        Uri url_2 = Request.Url;
+
+        Response.Write("<script>alert('Value : " + s + " -- url:"+url+" -- user_agent:"+user_agent+" -- userhost:"+userhost+"')</script>");
+        Response.Write("URL Port: " + url_2.Port + "<br>");
+        Response.Write("URL Protocol: " + Server.HtmlEncode(url_2.Scheme) + "<br>");
+    }
+}
