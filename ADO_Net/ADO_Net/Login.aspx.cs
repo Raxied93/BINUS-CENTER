ea9197bd13621faa4efcc32adab8eb4ea0511511
tandasanyu <tandasanyu@gmail>
2019-04-22 18:29:07 +0700

    Update Login.aspx.cs
    update

--- ADO_Net/ADO_Net/Login.aspx.cs
+++ ADO_Net/ADO_Net/Login.aspx.cs
@@ -7,7 +7,7 @@ using System.Web;
 using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+    
 public partial class Halaman_Login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
